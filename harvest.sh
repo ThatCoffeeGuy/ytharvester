31e4e0a1a50eefca09b7e14bdac05667f5ff4234
CoffeeKing <tunhube@gmail.com>
2022-02-05 12:18:58 +0100

    Changed to yt-dlp

--- harvest.sh
+++ harvest.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 ts="[`date "+%Y-%m-%d %H:%M:%S"`]"
 if [ $# -lt 2 ]; then
-    echo "USAGE: harvester <channel or playlist URL> <friendly name> "
+    echo "USAGE: harvester <channel or playlist URL> <friendly name>"
     exit 1
 fi
-sizebefore=`du -skh .`
-curdir=$(pwd)
+curdir=`pwd`
 url=$1
 name=$2
 if [[ -d "$name" ]]
@@ -15,10 +14,11 @@ else
    echo "$ts Creating directory..." && mkdir -p $name && echo "$ts Done!" || echo "$ts ERROR!"
 fi
 echo "$ts Looking for a youtube-dl upgrade..."
-/usr/bin/youtube-dl/youtube-dl -U && echo "$tsExit 0" || echo "$ts Exit not 0, something went wrong."
+/usr/local/bin/yt-dlp -U && echo "$ts Exit 0" || echo "$ts Exit not 0, something went wrong."
 cd $name
+sizebefore=`du -skh .`
 echo "$ts Starting yt harvester for $url channel in $curdir..."
-/usr/bin/youtube-dl/youtube-dl $url --download-archive list.txt --ignore-errors --verbose
+/usr/local/bin/yt-dlp $url --download-archive list.txt --ignore-errors --verbose --cookies ytcookie
 echo "$ts Completed!"
 sizeafter=`du -skh .`
 echo "$ts Folder size before: $sizebefore"
